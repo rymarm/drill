630541bc3a37e4ea201928bc6ccfdb4c65502ab2
Rymar Maksym <mrymar@cybervisiontech.com>
2021-02-15 09:22:34 +0200

    MD-6110: CVE-2020-27216: Jetty vulnerability (#538)
    Upgrade Jetty from 9.3.28 to 9.4.36
    (cherry picked from commit b1ce9720b2de46e1ff3aebfaeb7353789f03774a)

--- drill-yarn/src/main/java/org/apache/drill/yarn/appMaster/http/WebServer.java
+++ drill-yarn/src/main/java/org/apache/drill/yarn/appMaster/http/WebServer.java
@@ -65,11 +65,9 @@ import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.SessionManager;
 import org.eclipse.jetty.server.SslConnectionFactory;
 import org.eclipse.jetty.server.UserIdentity;
 import org.eclipse.jetty.server.handler.ErrorHandler;
-import org.eclipse.jetty.server.session.HashSessionManager;
 import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -306,7 +304,10 @@ public class WebServer implements AutoCloseable {
   }
 
   /**
-   * It creates A {@link SessionHandler} which contains a {@link HashSessionManager}
+   * It creates a {@link SessionHandler} with a {@link org.eclipse.jetty.server.session.DefaultSessionCache}
+   * which has {@link org.eclipse.jetty.server.session.NullSessionDataStore}.
+   *
+   * All sessions are stored in memory and on server restart are all destroyed.
    *
    * @param config Drill configs
    * @param securityHandler Set of initparameters that are used by the Authentication
@@ -314,10 +315,10 @@ public class WebServer implements AutoCloseable {
    */
   private SessionHandler createSessionHandler(Config config,
       final SecurityHandler securityHandler) {
-    SessionManager sessionManager = new HashSessionManager();
-    sessionManager.setMaxInactiveInterval(
+    SessionHandler sessionHandler = new SessionHandler();
+    sessionHandler.setMaxInactiveInterval(
         config.getInt(DrillOnYarnConfig.HTTP_SESSION_MAX_IDLE_SECS));
-    sessionManager.addEventListener(new HttpSessionListener() {
+    sessionHandler.addEventListener(new HttpSessionListener() {
       @Override
       public void sessionCreated(HttpSessionEvent se) {
         // No-op
@@ -340,7 +341,7 @@ public class WebServer implements AutoCloseable {
       }
     });
 
-    return new SessionHandler(sessionManager);
+    return sessionHandler;
   }
 
   /**
